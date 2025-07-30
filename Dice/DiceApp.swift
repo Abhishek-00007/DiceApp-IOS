fc9524801f422fd3a4032949c7b7815e2c2d1a12
Abhishek-00007 <p.abhishek.0007@gmail.com>
2025-07-30 22:10:01 +0530

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Abhishek Pandey on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceTests/DiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceTests.swift
+//  DiceTests
+//
+//  Created by Abhishek Pandey on 30/07/25.
+//
+
+import Testing
+@testable import Dice
+
+struct DiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
